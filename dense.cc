382c5df8ff39880b8f1f21a197fee1dc7386d278
jesper <jesper@taxboel.dk>
2018-03-11 04:25:05 +0100

    implemented GPU versions of dense

--- dense.cc
+++ dense.cc
@@ -10,7 +10,9 @@
 
 using namespace tensorflow;
 
-void DenseKernelLauncher(const double* in, const int N, double* out);
+/*
+    Register Dense operation
+*/
 
 REGISTER_OP("Dense")
   .Input("input: double")
@@ -18,8 +20,6 @@ REGISTER_OP("Dense")
   .Input("biases: double")
   .Output("output: double")
   .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
-
-    //printf("register op!!! \n");
 
     shape_inference::ShapeHandle input_shape;
     TF_RETURN_IF_ERROR(c->WithRank(c->input(0), 2, &input_shape));
@@ -30,10 +30,6 @@ REGISTER_OP("Dense")
     shape_inference::ShapeHandle biases_shape;
     TF_RETURN_IF_ERROR(c->WithRank(c->input(2), 2, &biases_shape));
     
-    
-
-    //shape_inference::DimensionHandle output_rows = c->Dim(weight_shape, 1);
-    
     shape_inference::DimensionHandle samples = c->Dim(input_shape, 0);
     shape_inference::DimensionHandle units = c->Dim(weight_shape, 1);
     
@@ -42,13 +38,17 @@ REGISTER_OP("Dense")
     return Status::OK();
   });
 
-class DenseOp : public OpKernel {
+/*
+    Dense Operation CPU
+*/
+
+class DenseOpCPU : public OpKernel {
 public:
-  explicit DenseOp(OpKernelConstruction* context) : OpKernel(context) {
+  explicit DenseOpCPU(OpKernelConstruction* context) : OpKernel(context) {
   }
   
   void Compute(OpKernelContext* context) override {
-    //printf("DenseOp-Begin\n");
+    //printf("DenseOpCPU\n");
 
     // get the input tensor
     const Tensor& input = context->input(0);
@@ -101,15 +101,6 @@ public:
     auto biases_tensor = biases.matrix<double>();
     auto output_tensor = output->matrix<double>();
     
-    // Set all but the first element of the output tensor to 0.
-    const int N = 1;
-    auto f_input = input.flat<double>();
-    auto f_output = output->template flat<double>();
-
-    // Call the cuda kernel launcher
-    //DenseKernelLauncher(f_input.data(), N, f_output.data());
-
-    
     for (int ix_sample = 0; ix_sample < batch_samples; ix_sample++) {
       for (int ix_unit = 0; ix_unit < units; ix_unit++) {
         output_tensor(ix_sample, ix_unit) = 0;
@@ -119,11 +110,104 @@ public:
         output_tensor(ix_sample, ix_unit) += biases_tensor(0, ix_unit);
       }
     }
-    
   }
 };
 
-REGISTER_KERNEL_BUILDER(Name("Dense").Device(DEVICE_CPU), DenseOp);
+REGISTER_KERNEL_BUILDER(Name("Dense").Device(DEVICE_CPU), DenseOpCPU);
+
+/*
+    Dense Operation GPU
+*/
+
+void DenseKernelLauncher(
+        const double* inputs, 
+        const double* weights,
+        const double* biases,
+        const int batch_samples, 
+        const int units, 
+        const int input_feature_width,
+        double* output);
+
+
+
+class DenseOpGPU : public OpKernel {
+public:
+  explicit DenseOpGPU(OpKernelConstruction* context) : OpKernel(context) {
+  }
+  
+  void Compute(OpKernelContext* context) override {
+    //printf("DenseOpGPU\n");
+
+    // get the input tensor
+    const Tensor& input = context->input(0);
+    
+    // get the weight tensor
+    const Tensor& weights = context->input(1);
+      
+    // get the bias tensor
+    const Tensor& biases = context->input(2);
+    
+    // check shapes of input and weights
+    const TensorShape& input_shape = input.shape();
+    const TensorShape& weights_shape = weights.shape();
+    const TensorShape& biases_shape = biases.shape();
+    
+    //Check that inputs are two dimensional
+    DCHECK_EQ(input_shape.dims(), 2);
+    DCHECK_EQ(weights_shape.dims(), 2);
+    DCHECK_EQ(biases_shape.dims(), 2);
+    
+    const int batch_samples = input_shape.dim_size(0);
+    //printf("batch_samples %d\n", batch_samples);
+
+    const int input_feature_width = input_shape.dim_size(1);
+    //printf("input_feature_width %d\n", input_feature_width);
+
+    const int units = weights_shape.dim_size(1);
+    //printf("units %d\n", units);
+
+    //Check input width matches weights height 
+    DCHECK_EQ(input_feature_width, weights_shape.dim_size(0));
+    //Check weights width match bias width 
+    DCHECK_EQ(weights_shape.dim_size(1), biases_shape.dim_size(1));
+
+    // create output shape
+    TensorShape output_shape;
+    //printf("batch_samples: %d\n", batch_samples);
+    //printf("units: %d\n", units);
+
+    output_shape.AddDim(batch_samples);
+    output_shape.AddDim(units);
+            
+    // create output tensor
+    Tensor* output = NULL;
+    OP_REQUIRES_OK(context, context->allocate_output(0, output_shape, &output));
+    
+    auto f_input = input.flat<double>();
+    auto f_weights = weights.flat<double>();
+    auto f_biases = biases.flat<double>();
+    auto f_output = output->template flat<double>();
+
+    DenseKernelLauncher(
+            f_input.data(), 
+            f_weights.data(),
+            f_biases.data(),
+            batch_samples, 
+            units, 
+            input_feature_width,
+            f_output.data()
+        );
+  }
+};
+
+REGISTER_KERNEL_BUILDER(Name("Dense").Device(DEVICE_GPU), DenseOpGPU);
+
+
+
+/*
+    DenseGrad Operation CPU
+*/
+
 
 REGISTER_OP("DenseGrad")
   .Input("grad: double")
@@ -134,14 +218,15 @@ REGISTER_OP("DenseGrad")
   .Output("grad_weights: double")
   .Output("grad_biases: double");
 
-class DenseGradOp : public OpKernel {
+class DenseGradOpCPU : public OpKernel {
 public:
-  explicit DenseGradOp(OpKernelConstruction* context) : OpKernel(context) {
+  explicit DenseGradOpCPU(OpKernelConstruction* context) : OpKernel(context) {
     
   }
   
   void Compute(OpKernelContext* context) override {
-    
+    //printf("DenseGradOpCPU\n");
+  
     DCHECK_EQ(4, context->num_inputs());
 
     const Tensor& grad = context->input(0);
@@ -177,12 +262,12 @@ public:
     int input_feature_width = input_shape.dim_size(1);  //Number of values in each sample
     int batch_samples = input_shape.dim_size(0); //Number of samples in batch
     int units = weights_shape.dim_size(1); //Number of units
+
     
     for (int x = 0; x < units; x++) 
     {
         grad_biases_tensor(0, x) = 0.0;
     }
-
     for (int x = 0; x < units; x++) //unit index 
     {
         for (int y = 0; y < input_feature_width; y++) //input feature index
@@ -190,7 +275,6 @@ public:
             grad_weights_tensor(y, x) = 0.0;
         }
     }
-    
     for (int x = 0; x < input_feature_width; x++) 
     {
         for (int y = 0; y < batch_samples; y++) 
@@ -214,5 +298,67 @@ public:
   }
 };
 
-REGISTER_KERNEL_BUILDER(Name("DenseGrad").Device(DEVICE_CPU), DenseGradOp);
+REGISTER_KERNEL_BUILDER(Name("DenseGrad").Device(DEVICE_CPU), DenseGradOpCPU);
+
+
+/*
+    DenseGrad Operation GPU
+*/
+
+void InputGradKernelLauncher(const double* grads, const double* weights, const int input_feature_width, const int batch_samples, const int units, double* grad_inputs);
+void WeightsGradKernelLauncher(const double* grads, const double* inputs, const int input_feature_width, const int batch_samples, const int units, double* grad_weights);
+void BiasesGradKernelLauncher(const double* grads, const int input_feature_width, const int batch_samples, const int units, double* grad_biases);
+  
+class DenseGradOpGPU : public OpKernel {
+public:
+  explicit DenseGradOpGPU(OpKernelConstruction* context) : OpKernel(context) {
+    
+  }
+  
+  void Compute(OpKernelContext* context) override {
+  
+    //printf("DenseGradOpGPU\n");
+  
+    DCHECK_EQ(4, context->num_inputs());
+
+    const Tensor& grad = context->input(0);
+    
+    const Tensor& input = context->input(1);
+    
+    const Tensor& weights = context->input(2);
+    
+    const Tensor& biases = context->input(3);
+    
+    TensorShape grad_shape = grad.shape();
+    TensorShape input_shape = input.shape();
+    TensorShape weights_shape = weights.shape();
+    TensorShape biases_shape = biases.shape();
+    
+    // create output tensors
+    Tensor* grad_input = NULL;
+    Tensor* grad_weights = NULL;
+    Tensor* grad_biases = NULL;
+    OP_REQUIRES_OK(context, context->allocate_output(0, input_shape, &grad_input));
+    OP_REQUIRES_OK(context, context->allocate_output(1, weights_shape, &grad_weights));
+    OP_REQUIRES_OK(context, context->allocate_output(2, biases_shape, &grad_biases));
+    
+    int input_feature_width = input_shape.dim_size(1);  //Number of values in each sample
+    int batch_samples = input_shape.dim_size(0); //Number of samples in batch
+    int units = weights_shape.dim_size(1); //Number of units
+    
+    auto f_grad = grad.flat<double>();
+    auto f_input = input.flat<double>();
+    auto f_weights = weights.flat<double>();
+    auto f_biases = biases.flat<double>();
+    auto f_grad_input = grad_input->template flat<double>();
+    auto f_grad_weights = grad_weights->template flat<double>();
+    auto f_grad_biases = grad_biases->template flat<double>();
+    
+    InputGradKernelLauncher(f_grad.data(), f_weights.data(), input_feature_width,batch_samples,units,f_grad_input.data());
+    WeightsGradKernelLauncher(f_grad.data(), f_input.data(), input_feature_width,batch_samples,units,f_grad_weights.data());
+    BiasesGradKernelLauncher(f_grad.data(), input_feature_width,batch_samples,units,f_grad_biases.data());
+  }
+};
+
+REGISTER_KERNEL_BUILDER(Name("DenseGrad").Device(DEVICE_GPU), DenseGradOpGPU);
 
--- dense_tests.py
+++ dense_tests.py
@@ -10,18 +10,29 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 class DenceOpTest(unittest.TestCase):
+    
     def test_dense(self):
-        with tf.Session(''):
-            #input feature width 1
-            #batch count 2
-            #units 3
+        #input feature width 1
+        #batch count 2
+        #units 3
+        
+        with tf.Session('') as sess:
+            x = tf.placeholder(tf.float64, shape = (2,1))
+            W = tf.constant(np.asarray([[1,2,3]]).astype(np.float64))
+            b = tf.constant(np.asarray([[0.1,0.2,0.3]]).astype(np.float64))
+            dense = dense_module.dense(x, W, b)
+            o = sess.run(dense, feed_dict = {x: np.asarray([[1], [2]]).astype(np.float64)})
+            print(o)
+            
+
             d_input = [[1], [2]]
             d_W = [[1, 2, 3]]
             d_b = [[0.1,0.2,0.3]]
             d = dense_module.dense(d_input, d_W, d_b)
-            ret = d.eval()
-            
-    def test_denseGrad(self):
+            o = d.eval()
+            print(o)
+
+    def test_denseGrad_cpu(self):
         with tf.Session('') as sess:
             x = tf.placeholder(tf.float64, shape = (2,1))
             W = tf.constant(np.asarray([[1,2,3]]).astype(np.float64))
@@ -29,65 +40,78 @@ class DenceOpTest(unittest.TestCase):
             Wx_dense = dense_module.dense(x, W, b)
             grad_x_dense = tf.gradients(Wx_dense, x)
             gradient_dense = sess.run(grad_x_dense, feed_dict = {x: np.asarray([[1], [2]]).astype(np.float64)})
-            
-    def test_gradientCheck(self):
+    
+
+    
+    def test_gradientCheck_cpu(self):
         with tf.Session('') as sess:
-            #decide dimensions
-            units = 10
-            samples = 21
-            inputs = 30 
+            for device in ['/cpu:0', '/gpu:0']:
+                with tf.device(device):
+                    #decide dimensions
+                    units = 2
+                    samples = 3
+                    inputs = 4 
 
-            print("Units:",units,"Samples:",samples,"Inputs:",inputs)
-            
-            n = samples*inputs + inputs*units+units
-            print("parameters:", n)
+                    print("Units:",units,"Samples:",samples,"Inputs:",inputs)
+                    
+                    n = samples*inputs + inputs*units+units
+                    print("parameters:", n)
 
-            #create graph
-            I = tf.placeholder(tf.float64, shape = (n))
-            x,W,b = tf.split(I, [samples*inputs,units*inputs,units])
-            x = tf.reshape(x,(samples,inputs))
-            W = tf.reshape(W,(inputs,units))
-            b = tf.reshape(b,(1,units))
+                    #create graph
+                    I = tf.placeholder(tf.float64, shape = (n))
+                    x,W,b = tf.split(I, [samples*inputs,units*inputs,units])
+                    x = tf.reshape(x,(samples,inputs))
+                    W = tf.reshape(W,(inputs,units))
+                    b = tf.reshape(b,(1,units))
 
-            print("x:", x)
-            print("W:", W)
-            print("b:", b)
+                    print("x:", x)
+                    print("W:", W)
+                    print("b:", b)
 
-            #new function
-            y = dense_module.dense(x, W, b)
-            #existing ops
-            #y = tf.matmul(x,W)+b     
-            #deadsimple
-            #y = tf.reduce_sum(x) + tf.reduce_sum(W) + tf.reduce_sum(b)
+                    #new function
+                    y = dense_module.dense(x, W, b)
+                    #existing ops
+                    #y = tf.matmul(x,W)+b     
+                    #deadsimple
+                    #y = tf.reduce_sum(x) + tf.reduce_sum(W) + tf.reduce_sum(b)
 
-            y = tf.reduce_sum(y)
-            print("y:", y)
-            
-            dydI = tf.gradients(y,I)
+                    y = tf.reduce_sum(y)
+                    print("y:", y)
+                    
+                    dydI = tf.gradients(y,I)
 
-            print("dydI:", dydI)
+                    print("dydI:", dydI)
 
-            testdata = np.random.rand(n).astype(np.float64)-0.5
-            epsilon = 10e-7
-            dydi = np.asarray(sess.run(dydI, feed_dict = {I: testdata})).flatten()
-            #print("Gradient analytically: ", dydi.shape)
-            dydi_approx = np.zeros((n,),np.float64)
-            for i in range(n):
-                tmp = testdata[i]
-                testdata[i] = tmp - epsilon
-                y0 = sess.run(y, feed_dict = {I: testdata})
-                testdata[i] = tmp + epsilon
-                y1 = sess.run(y, feed_dict = {I: testdata})
-                testdata[i] = tmp
-                y0 = y0.flatten()
-                y1 = y1.flatten()
-                
-                dydi_approx[i] = (y1-y0)/(2*epsilon)
-            
-            #print("dydi_approx:", dydi_approx.shape)
+                    testdata = np.random.rand(n).astype(np.float64)-0.5
+                    epsilon = 10e-7
+                    dydi = np.asarray(sess.run(dydI, feed_dict = {I: testdata})).flatten()
+                    #print("Gradient analytically: ", dydi.shape)
+                    dydi_approx = np.zeros((n,),np.float64)
+                    for i in range(n):
+                        tmp = testdata[i]
+                        testdata[i] = tmp - epsilon
+                        y0 = sess.run(y, feed_dict = {I: testdata})
+                        testdata[i] = tmp + epsilon
+                        y1 = sess.run(y, feed_dict = {I: testdata})
+                        testdata[i] = tmp
+                        y0 = y0.flatten()
+                        y1 = y1.flatten()
+                        
+                        dydi_approx[i] = (y1-y0)/(2*epsilon)
+                    
+                    #print("dydi_approx:", dydi_approx.shape)
 
-            gradient_error = np.linalg.norm(dydi_approx-dydi)/(np.linalg .norm(dydi_approx)+np.linalg.norm(dydi)) 
-            print("Gradient error is %0.16f"%( gradient_error))
+                    gradient_error = np.linalg.norm(dydi_approx-dydi)/(np.linalg .norm(dydi_approx)+np.linalg.norm(dydi)) 
+                    
+                    gradient_eval = "worry much, something is wrong"
+                    if gradient_error < 10e-7:
+                        gradient_eval = "great"
+                    elif gradient_error < 10e-5:
+                        gradient_eval = "doublecheck for bugs"
+                    
+
+                    print("Gradient error on %s is %e, Evaluation: %s"%(device, gradient_error, gradient_eval))
+        
                 
 if __name__ == '__main__':
     unittest.main()
